74447f7f324b1feeec116993211b7b9e99609565
Krishna Khairnar <91431849+KhairnarKrishna@users.noreply.github.com>
2024-04-30 13:30:53 +0530

    Adds  serializers, signals and urls, backend logic for VMS application

--- vms/settings.py
+++ vms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ljz$lq2$-coi*zmwu8!kwp2z=b@3rgnr%c1_3sm8+5tf(zmcm1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core',
 ]
 
 MIDDLEWARE = [
